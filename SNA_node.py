8c27ce7b04fa28c09286eafce55647a8be36beae
multiangle <461445957@qq.com>
2016-07-08 22:05:37 +0800

    社交网络预分析

--- SNA_node.py
+++ SNA_node.py
@@ -1,6 +1,13 @@
 __author__ = 'multiangle'
 class SNA_node():
     def __init__(self, content_dict):
+
+        self.next = []
+        self.isHeadNode = True
+        self.hasPre = False
+        self.preId = None
+        self.id = None # 后面会赋值
+
         item_list = [
             'created_at',
             'created_timestamp',
@@ -17,21 +24,27 @@ class SNA_node():
         for item in item_list:
             if item in content_key:
                 self.data[item] = content_dict[item]
-        if 'retweeted_status' in content_key:
+        self.id = self.data['id']
+
+        if 'retweeted_status' in content_key: # 处理转载的信息
             retweet_keys = list(content_dict['retweeted_status'].keys())
             self.data['retweeted_status'] = {}
             retweet_item_list = [
                 'created_at',
+                'created_timestamp',
                 'user_name',
                 'user_id',
-                'created_timestamp',
                 'id',
                 'idstr',
                 ]
             for item in retweet_item_list:
                 if item in retweet_keys:
                     self.data['retweeted_status'][item] = content_dict['retweeted_status'][item]
-        if 'user' in content_key:
+            self.hasPre = True
+            self.preId = self.data['retweeted_status']['id']
+            self.isHeadNode = False
+
+        if 'user' in content_key:  # 处理作者的信息
             user_keys = list(content_dict['user'].keys())
             self.data['user'] = {}
             user_item_list = [
@@ -46,3 +59,40 @@ class SNA_node():
             for item in user_item_list:
                 if item in user_keys:
                     self.data['user'][item] = content_dict['user'][item]
+
+
+    def add_Next(self, SNA_node):
+        self.next.append(SNA_node)
+
+    def build_Head_Node(retweeted_content):
+        node = SNA_node(retweeted_content)
+        return node
+
+    def analyse(self):
+        result = {}
+        result['retweet_num'] = self.next.__len__()
+        for msg in self.next : # 递归统计
+            msg.analyse()
+
+        if self.next.__len__()==0 : # 统计 转载深度
+            result['retweet_depth'] = 0
+        else:
+            max = 0
+            for msg in self.next:
+                if max<msg.result['retweet_depth'] :
+                    max = msg.result['retweet_depth']
+            result['retweet_depth'] = max + 1
+
+        if self.next.__len__()==0 : # 统计 有记录的转载次数
+            result['retweet_times'] = 0
+        else:
+            sum = 0
+            for msg in self.next:
+                sum += msg.result['retweet_times']
+            result['retweet_depth'] = sum
+
+        self.result = result
+
+
+    def __str__(self):
+        return self.data
--- SNA_pre.py
+++ SNA_pre.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 import json
 import File_Interface as FI
 from SNA_node import SNA_node
+import copy
 
 ### 将转载的内容贴出来
 # client = MongoClient('localhost',27017)
@@ -31,12 +32,55 @@ from SNA_node import SNA_node
 #         valid= False
 # FI.save_pickle(retweeted_data,'./static/retweeted_data.pkl')
 
-# 去除其他信息
-data = FI.load_pickle('./static/retweeted_data.pkl')
-info_list = []
-for line in data:
-    node = SNA_node(line)
-    info_list.append(node)
+# # 去除其他信息
+# data = FI.load_pickle('./static/retweeted_data.pkl')
+# info_list = []
+# for line in data:
+#     node = SNA_node(line)
+#     info_list.append(node)
+# FI.save_pickle(info_list,'./static/info_list.pkl')
+
+# # 建立转载链接关系
+# info_list = FI.load_pickle('./static/info_list.pkl')
+# info_dict ={}
+# for i in info_list:
+#     info_dict[i.data['id']] = i
+# info_id_list = list(info_dict.keys())
+# head_id_list = list(info_dict.keys())
+# changed = True
+# ite_times = 1
+# while changed:
+#     changed = False
+#     undealed_head = copy.deepcopy(head_id_list)
+#     num = undealed_head.__len__()
+#     for id in undealed_head:
+#         item = info_dict[id]
+#         if item.hasPre:
+#             changed = True
+#             if item.data['retweeted_status']['id'] in info_id_list:
+#                 info_dict[item.data['retweeted_status']['id']].add_Next(item)
+#                 head_id_list.remove(id)
+#             else:
+#                 pre_node = SNA_node.build_Head_Node(item.data['retweeted_status'])
+#                 pre_node.add_Next(item)
+#                 info_dict[pre_node.id] = pre_node
+#                 info_id_list.append(pre_node.id)
+#                 head_id_list.append(pre_node.id)
+#                 head_id_list.remove(id)
+#     print(ite_times)
+#     ite_times += 1
+#     print(head_id_list.__len__())
+# FI.save_pickle(info_dict,'./static/dealed_info_list.pkl')
+
+# 统计
+data = FI.load_pickle('./static/dealed_info_list.pkl')
+id_list = list(data.keys())
+for id in id_list:
+    data[id].analyse()
+FI.save_pickle(data,'./static/analyse_info_list.pkl')
+
+
+
 
 
 
