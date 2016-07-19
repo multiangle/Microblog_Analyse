5ae70af858b6a756d2364976eeb259d610f3b43b
multiangle <461445957@qq.com>
2016-07-19 23:54:11 +0800

    加载红楼梦

--- text_word_static.py
+++ text_word_static.py
@@ -23,36 +23,41 @@ def read_text_to_list(path,encoding='gbk'):
             line = file.readline()
     return s
 
-path = 'E:\multiangle\Coding!\《斗破苍穹》全本精校版.txt'
-path = r'E:\multiangle\E-BOOK\KINDLE自带\10中国古典小说名著百部\0104《红楼梦》作者：曹雪芹\0104《红楼梦》作者：曹雪芹 - 副本.txt'
-s = read_text_to_list(path)
-pattern = r'第.+?回'
-content_by_chap = []
-content_by_chap.append({})
-current_chap = content_by_chap[-1]
-current_chap['text'] = []
-content_by_chap.append(current_chap)
-for line in s:
-    if line.__len__()>1:
-        if re.match(pattern,line):
-            # print(line)
-            content_by_chap.append({})
-            current_chap = content_by_chap[-1]
-            current_chap['text'] = []
-            current_chap['title'] = line
-        else:
-            # print(line)
-            current_chap['text'].append(line[2:])
+def read_HongLouMeng():
+    path = r'E:\multiangle\E-BOOK\KINDLE自带\10中国古典小说名著百部\0104《红楼梦》作者：曹雪芹\0104《红楼梦》作者：曹雪芹 - 副本.txt'
+    s = read_text_to_list(path)
+    pattern = r'第.+?回'
+    content_by_chap = []
+    content_by_chap.append({})
+    current_chap = content_by_chap[-1]
+    current_chap['text'] = []
+    content_by_chap.append(current_chap)
+    for line in s:
+        if line.__len__()>1:
+            if re.match(pattern,line):
+                # print(line)
+                content_by_chap.append({})
+                current_chap = content_by_chap[-1]
+                current_chap['text'] = []
+                current_chap['title'] = line
+            else:
+                # print(line)
+                current_chap['text'].append(line[2:])
+    fintal_content = []
+    for chap in content_by_chap:
+        key = chap.keys()
+        if 'title' in key and 'text' in key:
+            fintal_content.append(chap)
+    return fintal_content
 
-print(content_by_chap.__len__())
-for chap in content_by_chap:
-    key = chap.keys()
-    if 'title' in key and 'text' in key:
-        print('\n')
-        print(chap['title'])
-        print(chap['text'])
-
-
+data = read_HongLouMeng()
+base_timestamp = 1468937028
+count = 0
+for line in data:
+    count += 1
+    line['time'] = base_timestamp + count
+for line in data:
+    print(line)
 
 # stop_words = FI.load_pickle('.\static\stop_words.pkl')
 # wdict = FI.load_pickle('.\static\斗破dict_huffman.pkl')
@@ -67,3 +72,4 @@ for chap in content_by_chap:
 #                       hufflen = line['Huffman'].__len__(),
 #                       f = line['freq']
 #         ))
+
